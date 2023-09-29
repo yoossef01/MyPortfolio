ac0a55f5975fd04c0326db05cfd893bcbef201b6
yoossef01 <105297325+yoossef01@users.noreply.github.com>
2023-09-29 21:54:59 +0100

    publish

--- src/app/components/content/contact/contact.component.ts
+++ src/app/components/content/contact/contact.component.ts
@@ -16,9 +16,9 @@ export class ContactComponent implements OnInit{
   subject!:string;
   text!:string;
   constructor() {
-    // Object.values(this.formGroup.controls).forEach(control => {
-    //   this.watchFormControlValue(control);
-    // })
+    Object.values(this.formGroup.controls).forEach(control => {
+      this.watchFormControlValue(control);
+    })
   }
   ngOnInit() {
     mailjs.init({
@@ -26,87 +26,87 @@ export class ContactComponent implements OnInit{
       password: 'votre_mot_de_passe'
     });
   }
-  // watchFormControlValue(control: AbstractControl) {
-  //   control.valueChanges.subscribe((controlValue) => {
-  //     if (control.dirty && controlValue === '') {
-  //       control.markAsPristine();
-  //     }
-  //   })
-  // }
-  sendEmail() {
-    const message = {
-      to: this.to,
-      subject: this.subject,
-      text: this.text
-    };
-  
-    mailjs.send(message, (error: any, info: any) => {
-      if (error) {
-        console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
-      } else {
-        console.log('E-mail envoyé avec succès :', info);
+  watchFormControlValue(control: AbstractControl) {
+    control.valueChanges.subscribe((controlValue) => {
+      if (control.dirty && controlValue === '') {
+        control.markAsPristine();
       }
-    });
+    })
+  }
+  // sendEmail() {
+  //   const message = {
+  //     to: this.to,
+  //     subject: this.subject,
+  //     text: this.text
+  //   };
+  
+  //   mailjs.send(message, (error: any, info: any) => {
+  //     if (error) {
+  //       console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
+  //     } else {
+  //       console.log('E-mail envoyé avec succès :', info);
+  //     }
+  //   });
+  // }
+
+  mailSent: boolean = false;
+  value: string = '';
+
+  formGroup = new FormGroup({
+    name: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    }
+    ),
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    })
+    ,
+    company: new FormControl(''),
+    website: new FormControl(''),
+    message: new FormControl('', Validators.required),
+  })
+
+  submitForm() {
+    this.formGroup.disable()
+    this.sendMail()
   }
 
-  // mailSent: boolean = false;
-  // value: string = '';
 
-  // formGroup = new FormGroup({
-  //   name: new FormControl('', {
-  //     validators: [
-  //       Validators.required,
-  //       Validators.minLength(3)
-  //     ]
-  //   }
-  //   ),
-  //   email: new FormControl('', {
-  //     validators: [
-  //       Validators.required,
-  //       Validators.email
-  //     ]
-  //   })
-  //   ,
-  //   company: new FormControl(''),
-  //   website: new FormControl(''),
-  //   message: new FormControl('', Validators.required),
-  // })
+  async sendMail() {
+    let formData = new FormData();
+    formData.append('company', this.formGroup.value.company || '');
+    formData.append('website', this.formGroup.value.website || '');
+    formData.append('name', this.formGroup.value.name || '');
+    formData.append('message', this.formGroup.value.message || '');
+    formData.append('email', this.formGroup.value.email || '');
 
-  // submitForm() {
-  //   this.formGroup.disable()
-  //   this.sendMail()
-  // }
+    fetch('https://www.ingo-hermsen.de/_sendphp/send_mail.php',
+      {
+        method: 'POST',
+        body: formData
+      }
+    ).then((response) => {
+      this.formGroup.reset()
+      this.showMailSentMessage()
 
+    })
 
-  // async sendMail() {
-  //   let formData = new FormData();
-  //   formData.append('company', this.formGroup.value.company || '');
-  //   formData.append('website', this.formGroup.value.website || '');
-  //   formData.append('name', this.formGroup.value.name || '');
-  //   formData.append('message', this.formGroup.value.message || '');
-  //   formData.append('email', this.formGroup.value.email || '');
+  }
 
-  //   fetch('https://www.ingo-hermsen.de/_sendphp/send_mail.php',
-  //     {
-  //       method: 'POST',
-  //       body: formData
-  //     }
-  //   ).then((response) => {
-  //     this.formGroup.reset()
-  //     this.showMailSentMessage()
-
-  //   })
-
-  // }
-
-  // showMailSentMessage() {
-  //   this.mailSent = true;
-  //   let mailSentTimeout = setTimeout(() => {
-  //     this.formGroup.enable();
-  //     this.mailSent = false;
-  //     clearTimeout(mailSentTimeout);
-  //   }, 3000)
-  // }
+  showMailSentMessage() {
+    this.mailSent = true;
+    let mailSentTimeout = setTimeout(() => {
+      this.formGroup.enable();
+      this.mailSent = false;
+      clearTimeout(mailSentTimeout);
+    }, 3000)
+  }
 
 }
 
